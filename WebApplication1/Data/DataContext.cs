c74a93cdda01b03109ff39bb89deda06b28d56af
B45i <amalshajan2011@gmail.com>
2019-01-11 00:18:00 +0530

    inital commit

---
+++ WebApplication1/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using System.Data;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Value> Values { get; set; }
+    }
+}
