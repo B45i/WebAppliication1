a5aa25f9fadc5c4be2c6d35fbd35692002b4812e
B45i <amalshajan2011@gmail.com>
2019-01-15 23:22:31 +0530

    authorization middleware

--- WebApplication1/Controllers/ValuesController.cs
+++ WebApplication1/Controllers/ValuesController.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebApplication1.Data;
 
 namespace WebApplication1.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -20,6 +22,7 @@ namespace WebApplication1.Controllers
         }
         
         // GET api/values
+        [Authorize]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
@@ -28,6 +31,7 @@ namespace WebApplication1.Controllers
         }
 
         // GET api/values/5
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
